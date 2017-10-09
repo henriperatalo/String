faf1506742a6b5097bd6eaebead9ec6233c69acc
henriperatalo <32446796+henriperatalo@users.noreply.github.com>
2017-10-09 08:59:59 +0300

    Update string.h

--- string.h
+++ string.h
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 
+//calculates length of string
 int my_strlen(char *c);
+//copies a string
 void mystrcpy(char *s,char *d);
+//compares two strings
 int mystrcmp (char *s, char *d);
+//switches all characters to uppercase
 int str2upper(char *s);
+//switches all characters to lowercase
 int str2lower_(char *s);
+//removes numbers from string
 int str_strip_numbers(char *s);
+//duplicates a string
 char *strdupl(char *s);
